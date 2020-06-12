b17f7931792d0ed2e045fa4b20b8cf33f010e7a3
Kim Fisher <kfisher@wcs.org>
2020-06-12 18:21:47 -0400

    first cut at wiring up prob logic

--- src/scl_probability_coefficients.py
+++ src/scl_probability_coefficients.py
@@ -1,12 +1,15 @@
 import os
 import argparse
 import pyodbc
+import numpy as np
 import pandas as pd
+from scipy.optimize import minimize
+from scipy.special import expit
 import ee
 from datetime import datetime, timezone
 from task_base import SCLTask
 from geomet import wkt
- 
+
 
 def fc2df(featurecollection):
     features = featurecollection.getInfo()["features"]
@@ -20,19 +23,17 @@ def fc2df(featurecollection):
 
 class SCLProbabilityCoefficients(SCLTask):
     ee_rootdir = "projects/SCL/v1"
+    ee_pocdir = "Panthera_tigris/geographies/Sumatra"
     inputs = {
-        "obs_adhoc": {"maxage": 10},
-        "obs_ss": {"maxage": 10},
-        "obs_ct": {"maxage": 10},
+        "obs_adhoc": {"maxage": 1},
+        "obs_ss": {"maxage": 1},
+        "obs_ct": {"maxage": 1},
         "hii": {
             "ee_type": SCLTask.IMAGECOLLECTION,
             "ee_path": "projects/HII/v1/hii",
-            "maxage": 1,
+            "maxage": 30,
         },
-        "dem": {
-            "ee_type": SCLTask.IMAGE,
-            "ee_path": "CGIAR/SRTM90_V4",
-        },  # no maxage; SRTM 20 years old
+        "dem": {"ee_type": SCLTask.IMAGE, "ee_path": "CGIAR/SRTM90_V4", "static": True},
         # TODO: replace with roads from OSM and calculate distance
         "roads": {
             "ee_type": SCLTask.FEATURECOLLECTION,
@@ -73,6 +74,13 @@ class SCLProbabilityCoefficients(SCLTask):
         )
         self.obsconn = pyodbc.connect(_obsconn_str)
 
+        self.Nx = 0
+        self.Nw = 0
+        self.Npsign = 0
+        self.NpCT = 0
+        self.W_back = None
+        self.X_back = None
+
     # could skip dataframes and go directly to numpy arrays, or use
     # https://pandas.pydata.org/pandas-docs/version/0.24.0rc1/api/generated/pandas.Series.to_numpy.html
     # depends on calculation needs
@@ -82,7 +90,8 @@ class SCLProbabilityCoefficients(SCLTask):
         if self._df_adhoc is None:
             query = (
                 f"SELECT * FROM dbo.vw_CI_AdHocObservation "
-                f"WHERE DATEDIFF(YEAR, ObservationDate, {self.taskdate}) <= {self.inputs['obs_adhoc']['maxage']}"
+                f"WHERE DATEDIFF(YEAR, ObservationDate, '{self.taskdate}') <= {self.inputs['obs_adhoc']['maxage']} "
+                f"AND ObservationDate <= Cast('{self.taskdate}' AS datetime)"
             )
             self._df_adhoc = pd.read_sql(query, self.obsconn)
         return self._df_adhoc
@@ -92,7 +101,8 @@ class SCLProbabilityCoefficients(SCLTask):
         if self._df_ct_dep is None:
             query = (
                 f"SELECT * FROM dbo.vw_CI_CameraTrapDeployment "
-                f"WHERE DATEDIFF(YEAR, PickupDatetime, {self.taskdate}) <= {self.inputs['obs_ct']['maxage']}"
+                f"WHERE DATEDIFF(YEAR, PickupDatetime, '{self.taskdate}') <= {self.inputs['obs_ct']['maxage']} "
+                f"AND PickupDatetime <= Cast('{self.taskdate}' AS datetime)"
             )
             self._df_ct_dep = pd.read_sql(query, self.obsconn)
         return self._df_ct_dep
@@ -102,7 +112,8 @@ class SCLProbabilityCoefficients(SCLTask):
         if self._df_ct_obs is None:
             query = (
                 f"SELECT * FROM dbo.vw_CI_CameraTrapObservation "
-                f"WHERE DATEDIFF(YEAR, ObservationDateTime, {self.taskdate}) <= {self.inputs['obs_ct']['maxage']}"
+                f"WHERE DATEDIFF(YEAR, ObservationDateTime, '{self.taskdate}') <= {self.inputs['obs_ct']['maxage']} "
+                f"AND ObservationDateTime <= Cast('{self.taskdate}' AS datetime)"
             )
             self._df_ct_obs = pd.read_sql(query, self.obsconn)
         return self._df_ct_obs
@@ -112,7 +123,8 @@ class SCLProbabilityCoefficients(SCLTask):
         if self._df_ss is None:
             query = (
                 f"SELECT * FROM dbo.vw_CI_SignSurveyObservation "
-                f"WHERE DATEDIFF(YEAR, StartDate, {self.taskdate}) <= {self.inputs['obs_ss']['maxage']}"
+                f"WHERE DATEDIFF(YEAR, StartDate, '{self.taskdate}') <= {self.inputs['obs_ss']['maxage']} "
+                f"AND StartDate <= Cast('{self.taskdate}' AS datetime)"
             )
             self._df_ss = pd.read_sql(query, self.obsconn)
         return self._df_ss
@@ -186,17 +198,194 @@ class SCLProbabilityCoefficients(SCLTask):
         else:
             return None
 
+    # def pbso_integrated(self):
+    #     """Overall function for optimizing function.
+    #
+    #     self.X_back: matrix with data for covariates that might affect tiger presence
+    #     self.W_back: matrix with data for covariates that might bias presence-only data
+    #     self.Npsign: single value sign survey
+    #     self.NpCT: single value camera trap
+    #
+    #     Returns dataframe of coefficients dataframe (parameter name, value, standard error),
+    #     convergence, message for optimization, and value of negative log-likelihood"""
+    #
+    #     beta_names = list(self.X_back)
+    #     # TODO: This is replacing the first string element of self.X_back, right? Why do this?
+    #     beta_names[0] = "beta0"
+    #     alpha_names = list(self.W_back)
+    #     alpha_names[0] = "alpha0"
+    #     psign_names = [f"p_sign_{i}" for i in range(0, self.Npsign)]
+    #     pcam_names = [f"p_cam_{i}" for i in range(0, self.NpCT)]
+    #     par_names = beta_names + alpha_names + psign_names + pcam_names
+    #     param_guess = np.zeros(len(par_names))
+    #     fit_pbso = minimize(self.neg_log_likelihood_int, param_guess, method="BFGS", options={"gtol": 1e-08})
+    #     se_pbso = np.zeros(len(fit_pbso.x))
+    #     # if fit_pbso.success==True:
+    #     #    se_pbso = np.sqrt(np.diag(fit_pbso.hess_inv))
+    #     tmp = {
+    #         "Parameter name": par_names,
+    #         "Value": fit_pbso.x,
+    #         "Standard error": se_pbso[0],
+    #     }
+    #     p = {
+    #         "coefs": pd.DataFrame(tmp, columns=["Parameter name", "Value", "Standard error"]),
+    #         "convergence": fit_pbso.success,
+    #         "optim_message": fit_pbso.message,
+    #         "value": fit_pbso.fun,
+    #     }
+    #     return p
+    #
+    # def neg_log_likelihood_int(self, par):
+    #     """Calculates the negative log-likelihood of the function.
+    #      Par: array list of parameters to optimize
+    #      Returns single value of negative log-likelihood of function"""
+    #
+    #     beta = par[0:self.Nx]
+    #     alpha = par[self.Nx: self.Nx + self.Nw]
+    #     p_sign = expit(par[self.Nx + self.Nw: self.Nx + self.Nw + self.Npsign])
+    #     p_cam = expit(par[self.Nx + self.Nw + self.Npsign: self.Nx + self.Nw + self.Npsign + self.NpCT])
+    #     lambda0 = np.exp(np.dot(np.array(self.X_back), beta))
+    #     psi = 1.0 - np.exp(-lambda0)
+    #     tw = np.dot(np.array(self.W_back), alpha)
+    #     p_thin = expit(tw)
+    #     zeta = np.empty((len(psi), 2))
+    #     zeta[:, 0] = 1.0 - psi
+    #     zeta[:, 1] = np.log(psi)
+    #
+    #     # TODO: refactor out "cell": either use dicts instead of lists, or sort and use df.index
+    #     # TODO: need to map db view CT data structure to this
+    #     for i in range(0, len(CT["det"])):
+    #         zeta[CT["cell"][i] - 1, 1] = (
+    #                 zeta[CT["cell"][i] - 1, 1]
+    #                 + (CT["det"][i]) * np.log(p_cam[CT["PI"][i] - 1])
+    #                 + (CT["days"][i] - CT["det"][i]) * np.log(1.0 - p_cam[CT["PI"][i] - 1])
+    #         )
+    #
+    #     for j in range(0, len(self.df_signsurvey["detections"])):
+    #         zeta[self.df_signsurvey["cell"][j] - 1, 1] = (
+    #                 zeta[self.df_signsurvey["cell"][j] - 1, 1]
+    #                 + (self.df_signsurvey["detections"][j]) * np.log(p_sign[self.df_signsurvey["SignSurveyID"][j] - 1])
+    #                 + (self.df_signsurvey["NumberOfReplicatesSurveyed"][j] - self.df_signsurvey["detections"][j])
+    #                 * np.log(1.0 - p_sign[self.df_signsurvey["SignSurveyID"][j] - 1])
+    #         )
+    #
+    #     # TODO: make variable names more readable
+    #     one = self.df_signsurvey[self.df_signsurvey["detections"] > 0]["cell"]
+    #     two = CT[CT["det"] > 0]["cell"]
+    #     known_occurrences = list(set(one.append(two)))
+    #
+    #     zeta[np.array(known_occurrences) - 1, 0] = 0
+    #
+    #     lik_so = []
+    #     for i in range(0, len(zeta[:, 0])):
+    #         if zeta[i, 0] == 0:
+    #             lik_so.append(zeta[i, 1])
+    #         else:
+    #             lik_so.append(np.log(zeta[i, 0]) + zeta[i, 1])
+    #
+    #     nll_po = -1.0 * (
+    #             -1.0 * sum(lambda0 * p_thin)
+    #             # TODO: I don't understand the values in po_data.csv; need to map df_adhoc to this
+    #             + sum(np.log(lambda0[po_data - 1] * p_thin[po_data - 1]))
+    #     )
+    #     nll_so = -1.0 * sum(lik_so)
+    #
+    #     return nll_po[0] + nll_so
+    #
+    # # TODO: DRY this up to avoid the repeated lines
+    # def predict_surface(self, par, griddata):
+    #     """Create predicted probability surface for each grid cell.
+    #      Par: list of parameter values that have been optimized to convert to probability surface
+    #      Returns data frame that includes grid code, grid cell number and predicted probability surface for each grid
+    #      cell"""
+    #
+    #     par = np.array(par)
+    #     beta = par[0:self.Nx]
+    #     p_sign = expit(par[self.Nx + self.Nw: self.Nx + self.Nw + self.Npsign])
+    #     p_cam = expit(par[self.Nx + self.Nw + self.Npsign: self.Nx + self.Nw + self.Npsign + self.NpCT])
+    #     lambda0 = np.exp(np.dot(np.array(self.X_back), beta))
+    #     psi = 1.0 - np.exp(-lambda0)
+    #     zeta = np.empty((len(psi), 2))
+    #     zeta[:, 0] = 1.0 - psi
+    #     zeta[:, 1] = np.log(psi)
+    #
+    #     for i in range(0, len(CT["det"])):
+    #         zeta[CT["cell"][i] - 1, 1] = (
+    #                 zeta[CT["cell"][i] - 1, 1]
+    #                 + (CT["det"][i]) * np.log(p_cam[CT["PI"][i] - 1])
+    #                 + (CT["days"][i] - CT["det"][i]) * np.log(1.0 - p_cam[CT["PI"][i] - 1])
+    #         )
+    #
+    #     for j in range(0, len(self.df_signsurvey["detections"])):
+    #         zeta[self.df_signsurvey["cell"][j] - 1, 1] = (
+    #                 zeta[self.df_signsurvey["cell"][j] - 1, 1]
+    #                 + (self.df_signsurvey["detections"][j]) * np.log(p_sign[self.df_signsurvey["SignSurveyID"][j] - 1])
+    #                 + (self.df_signsurvey["NumberOfReplicatesSurveyed"][j] - self.df_signsurvey["detections"][j])
+    #                 * np.log(1.0 - p_sign[self.df_signsurvey["SignSurveyID"][j] - 1])
+    #         )
+    #
+    #     one = self.df_signsurvey[self.df_signsurvey["detections"] > 0]["cell"]
+    #     two = CT[CT["det"] > 0]["cell"]
+    #     known_occurrences = list(set(one.append(two)))
+    #
+    #     zeta[np.array(known_occurrences) - 1, 0] = 0
+    #     cond_psi = [zeta[i, 1] / sum(zeta[i, :]) for i in range(0, len(psi))]
+    #
+    #     cond_prob = 1.0 - (1.0 - np.exp(np.multiply(-1, cond_psi)))
+    #     gridcells = [i for i in range(1, len(psi) + 1)]
+    #     temp = {"gridcellcode": griddata["gridcellcode"], "gridgridcells": gridcells, "condprob": cond_prob}
+    #     prob_out = pd.DataFrame(temp, columns=["gridcellcode", "gridgridcells", "condprob"])
+    #
+    #     return prob_out
+
     def calc(self):
-        print(self.df_adhoc)
-        print(self.df_cameratrap_dep)
-        print(self.df_cameratrap_obs)
+        # TODO: either we need to set these once per grid (filtering by grid id), and then combine the resulting grids,
+        #  or the prob functions need to treat the grids separately internally
+        # print(self.df_adhoc)
+        # print(self.df_cameratrap_dep)
+        # print(self.df_cameratrap_obs)
         print(self.df_signsurvey)
+        # cell_totals = self.df_signsurvey.groupby(['GridCellCode'])['detections'].sum()
+
+        # prob_images = []
         for gridname in self.grids.keys():
-            df_covars = self.get_covariates(gridname)
-            print(df_covars)
+            # Getting from ee works but using cached csv to speed up development
+            # df_covars = self.get_covariates(gridname)
+            # df_covars.to_csv("covars.csv", encoding="utf-8", index=False)
+            # df_covars = pd.read_csv(
+            #     "covars.csv", encoding="utf-8", index_col="gridcellcode"
+            # )
+            # print(df_covars)
 
-        # TODO: calculate coefficients here (port R code)
+            # TODO: set these dynamically
+            self.Nx = 3
+            self.Nw = 3
+            self.Npsign = 1
+            self.NpCT = 1
 
+            # self.W_back = df_covars[["tri", "distance_to_roads"]]
+            # TODO: Why do we need this column?
+            # self.W_back.insert(0, "Int", 1)
+            # self.X_back = df_covars[["structural_habitat", "hii"]]
+            # self.X_back.insert(0, "Int", 1)
+
+            # m = self.pbso_integrated()
+            # probs = self.predict_surface(m["coefs"]["Value"], df_covars)
+
+            # def fake_prob_algorithm(f):
+            #
+            # # for each f in the fc, get # of observations that intersect
+            # fake_prob_fc = cell_features.map()
+            # # prob_images.append(ee.Image(1))
+            # fake_prob = cell_features.reduceToImage(['probability'], ee.Reducer.max())
+            # self.export_image_ee(fake_prob, f"{self.ee_pocdir}/hab/probability")
+
+        # TODO: add (? or otherwise combine) all probability images, one for each grid
+
+        # fake_prob = cell_features.reduceToImage(['probability'], ee.Reducer.max())
+        # self.export_image_ee(fake_prob, f"{self.ee_pocdir}/hab/probability")
+
+        # OUTDATED: we're going to output a probability surface directly
         # store results in ee table in form {"coeff": "<coeff name>", "value": <float | 0 | None>}
         # dummy_geom = ee.Geometry.Point([0, 0])  # can't export tables with undefined or null geometries
         # structural_habitat = ee.Feature(dummy_geom, {"coeff": "structural_habitat", "value": 9.543099})
