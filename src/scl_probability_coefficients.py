0c086ce1381ac4b80458e1143fcad8770f95222a
Kim Fisher <kfisher@wcs.org>
2020-06-13 17:56:04 -0400

    dumb probability export, pass grid-specific dfs to prob functions

--- src/scl_probability_coefficients.py
+++ src/scl_probability_coefficients.py
@@ -46,6 +46,7 @@ class SCLProbabilityCoefficients(SCLTask):
             "maxage": 1,
         },
     }
+    cell_label = "GridCellCode"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -148,8 +149,7 @@ class SCLProbabilityCoefficients(SCLTask):
                 df_gridcells = pd.read_sql(gridcells_query, self.obsconn)
                 gridcells_list = df_gridcells.values.tolist()
                 self._grids[gridname] = [
-                    ee.Feature(wkt.loads(g[1]), {"gridcellcode": g[0]})
-                    for g in gridcells_list
+                    (wkt.loads(g[1]), {self.cell_label: g[0]}) for g in gridcells_list
                 ]
         return self._grids
 
@@ -165,7 +165,7 @@ class SCLProbabilityCoefficients(SCLTask):
     # or to sample using smaller gridcell geometries
     def get_covariates(self, grid):
         try:
-            cells = self.grids[grid]
+            cells = [ee.Feature(g[0], g[1]) for g in self.grids[grid]]
         except KeyError:
             raise KeyError(f"No grid {grid} in observations")
         cell_features = ee.FeatureCollection(cells)
@@ -198,162 +198,203 @@ class SCLProbabilityCoefficients(SCLTask):
         else:
             return None
 
-    # def pbso_integrated(self):
-    #     """Overall function for optimizing function.
-    #
-    #     self.X_back: matrix with data for covariates that might affect tiger presence
-    #     self.W_back: matrix with data for covariates that might bias presence-only data
-    #     self.Npsign: single value sign survey
-    #     self.NpCT: single value camera trap
-    #
-    #     Returns dataframe of coefficients dataframe (parameter name, value, standard error),
-    #     convergence, message for optimization, and value of negative log-likelihood"""
-    #
-    #     beta_names = list(self.X_back)
-    #     # TODO: This is replacing the first string element of self.X_back, right? Why do this?
-    #     beta_names[0] = "beta0"
-    #     alpha_names = list(self.W_back)
-    #     alpha_names[0] = "alpha0"
-    #     psign_names = [f"p_sign_{i}" for i in range(0, self.Npsign)]
-    #     pcam_names = [f"p_cam_{i}" for i in range(0, self.NpCT)]
-    #     par_names = beta_names + alpha_names + psign_names + pcam_names
-    #     param_guess = np.zeros(len(par_names))
-    #     fit_pbso = minimize(self.neg_log_likelihood_int, param_guess, method="BFGS", options={"gtol": 1e-08})
-    #     se_pbso = np.zeros(len(fit_pbso.x))
-    #     # if fit_pbso.success==True:
-    #     #    se_pbso = np.sqrt(np.diag(fit_pbso.hess_inv))
-    #     tmp = {
-    #         "Parameter name": par_names,
-    #         "Value": fit_pbso.x,
-    #         "Standard error": se_pbso[0],
-    #     }
-    #     p = {
-    #         "coefs": pd.DataFrame(tmp, columns=["Parameter name", "Value", "Standard error"]),
-    #         "convergence": fit_pbso.success,
-    #         "optim_message": fit_pbso.message,
-    #         "value": fit_pbso.fun,
-    #     }
-    #     return p
-    #
-    # def neg_log_likelihood_int(self, par):
-    #     """Calculates the negative log-likelihood of the function.
-    #      Par: array list of parameters to optimize
-    #      Returns single value of negative log-likelihood of function"""
-    #
-    #     beta = par[0:self.Nx]
-    #     alpha = par[self.Nx: self.Nx + self.Nw]
-    #     p_sign = expit(par[self.Nx + self.Nw: self.Nx + self.Nw + self.Npsign])
-    #     p_cam = expit(par[self.Nx + self.Nw + self.Npsign: self.Nx + self.Nw + self.Npsign + self.NpCT])
-    #     lambda0 = np.exp(np.dot(np.array(self.X_back), beta))
-    #     psi = 1.0 - np.exp(-lambda0)
-    #     tw = np.dot(np.array(self.W_back), alpha)
-    #     p_thin = expit(tw)
-    #     zeta = np.empty((len(psi), 2))
-    #     zeta[:, 0] = 1.0 - psi
-    #     zeta[:, 1] = np.log(psi)
-    #
-    #     # TODO: refactor out "cell": either use dicts instead of lists, or sort and use df.index
-    #     # TODO: need to map db view CT data structure to this
-    #     for i in range(0, len(CT["det"])):
-    #         zeta[CT["cell"][i] - 1, 1] = (
-    #                 zeta[CT["cell"][i] - 1, 1]
-    #                 + (CT["det"][i]) * np.log(p_cam[CT["PI"][i] - 1])
-    #                 + (CT["days"][i] - CT["det"][i]) * np.log(1.0 - p_cam[CT["PI"][i] - 1])
-    #         )
-    #
-    #     for j in range(0, len(self.df_signsurvey["detections"])):
-    #         zeta[self.df_signsurvey["cell"][j] - 1, 1] = (
-    #                 zeta[self.df_signsurvey["cell"][j] - 1, 1]
-    #                 + (self.df_signsurvey["detections"][j]) * np.log(p_sign[self.df_signsurvey["SignSurveyID"][j] - 1])
-    #                 + (self.df_signsurvey["NumberOfReplicatesSurveyed"][j] - self.df_signsurvey["detections"][j])
-    #                 * np.log(1.0 - p_sign[self.df_signsurvey["SignSurveyID"][j] - 1])
-    #         )
-    #
-    #     # TODO: make variable names more readable
-    #     one = self.df_signsurvey[self.df_signsurvey["detections"] > 0]["cell"]
-    #     two = CT[CT["det"] > 0]["cell"]
-    #     known_occurrences = list(set(one.append(two)))
-    #
-    #     zeta[np.array(known_occurrences) - 1, 0] = 0
-    #
-    #     lik_so = []
-    #     for i in range(0, len(zeta[:, 0])):
-    #         if zeta[i, 0] == 0:
-    #             lik_so.append(zeta[i, 1])
-    #         else:
-    #             lik_so.append(np.log(zeta[i, 0]) + zeta[i, 1])
-    #
-    #     nll_po = -1.0 * (
-    #             -1.0 * sum(lambda0 * p_thin)
-    #             # TODO: I don't understand the values in po_data.csv; need to map df_adhoc to this
-    #             + sum(np.log(lambda0[po_data - 1] * p_thin[po_data - 1]))
-    #     )
-    #     nll_so = -1.0 * sum(lik_so)
-    #
-    #     return nll_po[0] + nll_so
-    #
-    # # TODO: DRY this up to avoid the repeated lines
-    # def predict_surface(self, par, griddata):
-    #     """Create predicted probability surface for each grid cell.
-    #      Par: list of parameter values that have been optimized to convert to probability surface
-    #      Returns data frame that includes grid code, grid cell number and predicted probability surface for each grid
-    #      cell"""
-    #
-    #     par = np.array(par)
-    #     beta = par[0:self.Nx]
-    #     p_sign = expit(par[self.Nx + self.Nw: self.Nx + self.Nw + self.Npsign])
-    #     p_cam = expit(par[self.Nx + self.Nw + self.Npsign: self.Nx + self.Nw + self.Npsign + self.NpCT])
-    #     lambda0 = np.exp(np.dot(np.array(self.X_back), beta))
-    #     psi = 1.0 - np.exp(-lambda0)
-    #     zeta = np.empty((len(psi), 2))
-    #     zeta[:, 0] = 1.0 - psi
-    #     zeta[:, 1] = np.log(psi)
-    #
-    #     for i in range(0, len(CT["det"])):
-    #         zeta[CT["cell"][i] - 1, 1] = (
-    #                 zeta[CT["cell"][i] - 1, 1]
-    #                 + (CT["det"][i]) * np.log(p_cam[CT["PI"][i] - 1])
-    #                 + (CT["days"][i] - CT["det"][i]) * np.log(1.0 - p_cam[CT["PI"][i] - 1])
-    #         )
-    #
-    #     for j in range(0, len(self.df_signsurvey["detections"])):
-    #         zeta[self.df_signsurvey["cell"][j] - 1, 1] = (
-    #                 zeta[self.df_signsurvey["cell"][j] - 1, 1]
-    #                 + (self.df_signsurvey["detections"][j]) * np.log(p_sign[self.df_signsurvey["SignSurveyID"][j] - 1])
-    #                 + (self.df_signsurvey["NumberOfReplicatesSurveyed"][j] - self.df_signsurvey["detections"][j])
-    #                 * np.log(1.0 - p_sign[self.df_signsurvey["SignSurveyID"][j] - 1])
-    #         )
-    #
-    #     one = self.df_signsurvey[self.df_signsurvey["detections"] > 0]["cell"]
-    #     two = CT[CT["det"] > 0]["cell"]
-    #     known_occurrences = list(set(one.append(two)))
-    #
-    #     zeta[np.array(known_occurrences) - 1, 0] = 0
-    #     cond_psi = [zeta[i, 1] / sum(zeta[i, :]) for i in range(0, len(psi))]
-    #
-    #     cond_prob = 1.0 - (1.0 - np.exp(np.multiply(-1, cond_psi)))
-    #     gridcells = [i for i in range(1, len(psi) + 1)]
-    #     temp = {"gridcellcode": griddata["gridcellcode"], "gridgridcells": gridcells, "condprob": cond_prob}
-    #     prob_out = pd.DataFrame(temp, columns=["gridcellcode", "gridgridcells", "condprob"])
-    #
-    #     return prob_out
+    def pbso_integrated(self):
+        """Overall function for optimizing function.
+
+        self.X_back: matrix with data for covariates that might affect tiger presence
+        self.W_back: matrix with data for covariates that might bias presence-only data
+        self.Npsign: single value sign survey
+        self.NpCT: single value camera trap
+
+        Returns dataframe of coefficients dataframe (parameter name, value, standard error),
+        convergence, message for optimization, and value of negative log-likelihood"""
+
+        beta_names = list(self.X_back)
+        # TODO: This is replacing the first string element of self.X_back, right? Why do this?
+        beta_names[0] = "beta0"
+        alpha_names = list(self.W_back)
+        alpha_names[0] = "alpha0"
+        psign_names = [f"p_sign_{i}" for i in range(0, self.Npsign)]
+        pcam_names = [f"p_cam_{i}" for i in range(0, self.NpCT)]
+        par_names = beta_names + alpha_names + psign_names + pcam_names
+        param_guess = np.zeros(len(par_names))
+        fit_pbso = minimize(
+            self.neg_log_likelihood_int,
+            param_guess,
+            method="BFGS",
+            options={"gtol": 1e-08},
+        )
+        se_pbso = np.zeros(len(fit_pbso.x))
+        # if fit_pbso.success==True:
+        #    se_pbso = np.sqrt(np.diag(fit_pbso.hess_inv))
+        tmp = {
+            "Parameter name": par_names,
+            "Value": fit_pbso.x,
+            "Standard error": se_pbso[0],
+        }
+        p = {
+            "coefs": pd.DataFrame(
+                tmp, columns=["Parameter name", "Value", "Standard error"]
+            ),
+            "convergence": fit_pbso.success,
+            "optim_message": fit_pbso.message,
+            "value": fit_pbso.fun,
+        }
+        return p
+
+    def neg_log_likelihood_int(self, par, CT, df_signsurvey, po_data):
+        """Calculates the negative log-likelihood of the function.
+         Par: array list of parameters to optimize
+         Returns single value of negative log-likelihood of function"""
+
+        beta = par[0 : self.Nx]
+        alpha = par[self.Nx : self.Nx + self.Nw]
+        p_sign = expit(par[self.Nx + self.Nw : self.Nx + self.Nw + self.Npsign])
+        p_cam = expit(
+            par[
+                self.Nx
+                + self.Nw
+                + self.Npsign : self.Nx
+                + self.Nw
+                + self.Npsign
+                + self.NpCT
+            ]
+        )
+        lambda0 = np.exp(np.dot(np.array(self.X_back), beta))
+        psi = 1.0 - np.exp(-lambda0)
+        tw = np.dot(np.array(self.W_back), alpha)
+        p_thin = expit(tw)
+        zeta = np.empty((len(psi), 2))
+        zeta[:, 0] = 1.0 - psi
+        zeta[:, 1] = np.log(psi)
+
+        # TODO: refactor out "cell": either use dicts instead of lists, or sort and use df.index
+        # TODO: need to map db view CT data structure to this
+        for i in range(0, len(CT["det"])):
+            zeta[CT["cell"][i] - 1, 1] = (
+                zeta[CT["cell"][i] - 1, 1]
+                + (CT["det"][i]) * np.log(p_cam[CT["PI"][i] - 1])
+                + (CT["days"][i] - CT["det"][i]) * np.log(1.0 - p_cam[CT["PI"][i] - 1])
+            )
+
+        for j in range(0, len(df_signsurvey["detections"])):
+            zeta[df_signsurvey["cell"][j] - 1, 1] = (
+                zeta[df_signsurvey["cell"][j] - 1, 1]
+                + (df_signsurvey["detections"][j])
+                * np.log(p_sign[df_signsurvey["SignSurveyID"][j] - 1])
+                + (
+                    df_signsurvey["NumberOfReplicatesSurveyed"][j]
+                    - df_signsurvey["detections"][j]
+                )
+                * np.log(1.0 - p_sign[df_signsurvey["SignSurveyID"][j] - 1])
+            )
+
+        # TODO: make variable names more readable
+        one = df_signsurvey[df_signsurvey["detections"] > 0]["cell"]
+        two = CT[CT["det"] > 0]["cell"]
+        known_occurrences = list(set(one.append(two)))
+
+        zeta[np.array(known_occurrences) - 1, 0] = 0
+
+        lik_so = []
+        for i in range(0, len(zeta[:, 0])):
+            if zeta[i, 0] == 0:
+                lik_so.append(zeta[i, 1])
+            else:
+                lik_so.append(np.log(zeta[i, 0]) + zeta[i, 1])
+
+        nll_po = -1.0 * (
+            -1.0 * sum(lambda0 * p_thin)
+            # TODO: I don't understand the values in po_data.csv; need to map df_adhoc to this
+            + sum(np.log(lambda0[po_data - 1] * p_thin[po_data - 1]))
+        )
+        nll_so = -1.0 * sum(lik_so)
+
+        return nll_po[0] + nll_so
+
+    # TODO: DRY this up to avoid the repeated lines
+    def predict_surface(self, par, CT, df_signsurvey, griddata):
+        """Create predicted probability surface for each grid cell.
+         Par: list of parameter values that have been optimized to convert to probability surface
+         Returns data frame that includes grid code, grid cell number and predicted probability surface for each grid
+         cell"""
+
+        par = np.array(par)
+        beta = par[0 : self.Nx]
+        p_sign = expit(par[self.Nx + self.Nw : self.Nx + self.Nw + self.Npsign])
+        p_cam = expit(
+            par[
+                self.Nx
+                + self.Nw
+                + self.Npsign : self.Nx
+                + self.Nw
+                + self.Npsign
+                + self.NpCT
+            ]
+        )
+        lambda0 = np.exp(np.dot(np.array(self.X_back), beta))
+        psi = 1.0 - np.exp(-lambda0)
+        zeta = np.empty((len(psi), 2))
+        zeta[:, 0] = 1.0 - psi
+        zeta[:, 1] = np.log(psi)
+
+        for i in range(0, len(CT["det"])):
+            zeta[CT["cell"][i] - 1, 1] = (
+                zeta[CT["cell"][i] - 1, 1]
+                + (CT["det"][i]) * np.log(p_cam[CT["PI"][i] - 1])
+                + (CT["days"][i] - CT["det"][i]) * np.log(1.0 - p_cam[CT["PI"][i] - 1])
+            )
+
+        for j in range(0, len(df_signsurvey["detections"])):
+            zeta[df_signsurvey["cell"][j] - 1, 1] = (
+                zeta[df_signsurvey["cell"][j] - 1, 1]
+                + (df_signsurvey["detections"][j])
+                * np.log(p_sign[df_signsurvey["SignSurveyID"][j] - 1])
+                + (
+                    df_signsurvey["NumberOfReplicatesSurveyed"][j]
+                    - df_signsurvey["detections"][j]
+                )
+                * np.log(1.0 - p_sign[df_signsurvey["SignSurveyID"][j] - 1])
+            )
+
+        one = df_signsurvey[df_signsurvey["detections"] > 0]["cell"]
+        two = CT[CT["det"] > 0]["cell"]
+        known_occurrences = list(set(one.append(two)))
+
+        zeta[np.array(known_occurrences) - 1, 0] = 0
+        cond_psi = [zeta[i, 1] / sum(zeta[i, :]) for i in range(0, len(psi))]
+
+        cond_prob = 1.0 - (1.0 - np.exp(np.multiply(-1, cond_psi)))
+        gridcells = [i for i in range(1, len(psi) + 1)]
+        temp = {
+            self.cell_label: griddata[self.cell_label],
+            "gridcells": gridcells,
+            "condprob": cond_prob,
+        }
+        prob_out = pd.DataFrame(
+            temp, columns=[self.cell_label, "gridcells", "condprob"]
+        )
+
+        return prob_out
 
     def calc(self):
-        # TODO: either we need to set these once per grid (filtering by grid id), and then combine the resulting grids,
-        #  or the prob functions need to treat the grids separately internally
-        # print(self.df_adhoc)
-        # print(self.df_cameratrap_dep)
-        # print(self.df_cameratrap_obs)
-        print(self.df_signsurvey)
-        # cell_totals = self.df_signsurvey.groupby(['GridCellCode'])['detections'].sum()
-
         # prob_images = []
         for gridname in self.grids.keys():
+            # just observations for this gridname, where cell labels can be used as index
+            # TODO: assuming just one survey per cell. Need to handle multiple surveys in prob functions.
+            df_adhoc = self.df_adhoc[self.df_adhoc["GridName"].str.match(gridname)]
+            # TODO: combine CT dep and obs dfs for prob functions
+            df_cameratrap_dep = self.df_cameratrap_dep[
+                self.df_cameratrap_dep["GridName"].str.match(gridname)
+            ]
+            df_signsurvey = self.df_signsurvey[
+                self.df_signsurvey["GridName"].str.match(gridname)
+            ]
             # Getting from ee works but using cached csv to speed up development
             # df_covars = self.get_covariates(gridname)
             # df_covars.to_csv("covars.csv", encoding="utf-8", index=False)
             # df_covars = pd.read_csv(
-            #     "covars.csv", encoding="utf-8", index_col="gridcellcode"
+            #     "covars.csv", encoding="utf-8", index_col=self.cell_label
             # )
             # print(df_covars)
 
@@ -369,21 +410,39 @@ class SCLProbabilityCoefficients(SCLTask):
             # self.X_back = df_covars[["structural_habitat", "hii"]]
             # self.X_back.insert(0, "Int", 1)
 
+            # TODO: pass grid-specific (and survey-specific?) dfs to prob functions
             # m = self.pbso_integrated()
-            # probs = self.predict_surface(m["coefs"]["Value"], df_covars)
+            # probs = self.predict_surface(m["coefs"]["Value"], df_cameratrap_dep, df_signsurvey, df_covars)
 
-            # def fake_prob_algorithm(f):
-            #
-            # # for each f in the fc, get # of observations that intersect
-            # fake_prob_fc = cell_features.map()
-            # # prob_images.append(ee.Image(1))
-            # fake_prob = cell_features.reduceToImage(['probability'], ee.Reducer.max())
-            # self.export_image_ee(fake_prob, f"{self.ee_pocdir}/hab/probability")
+            probcells = []
+            for cell in self.grids[gridname]:
+                gridcellcode = cell[1][self.cell_label]
+                detections = 0
+                try:
+                    detections = int(
+                        df_signsurvey[
+                            df_signsurvey[self.cell_label].str.match(gridcellcode)
+                        ]["detections"]
+                    )
+                    if detections > 1:
+                        detections = 1
+                except KeyError:
+                    pass
+
+                props = cell[1]
+                props["probability"] = detections
+                probcell = ee.Feature(cell[0], props)
+                probcells.append(probcell)
+
+            fake_prob = (
+                ee.FeatureCollection(probcells)
+                .reduceToImage(["probability"], ee.Reducer.max())
+                .rename("probability")
+            )
+            self.export_image_ee(fake_prob, f"{self.ee_pocdir}/hab/probability")
 
         # TODO: add (? or otherwise combine) all probability images, one for each grid
-
-        # fake_prob = cell_features.reduceToImage(['probability'], ee.Reducer.max())
-        # self.export_image_ee(fake_prob, f"{self.ee_pocdir}/hab/probability")
+        # self.export_image_ee(combined_images, f"{self.ee_pocdir}/hab/probability")
 
         # OUTDATED: we're going to output a probability surface directly
         # store results in ee table in form {"coeff": "<coeff name>", "value": <float | 0 | None>}
